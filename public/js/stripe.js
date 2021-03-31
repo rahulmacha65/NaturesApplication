6816d353b232c2c1f75a80743cd9a70e6f007ad7
rahulmacha65 <rahul.macha567@gmail.com>
2021-03-31 20:32:24 +0530

    Initial commit

---
+++ public/js/stripe.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+
+import axios from 'axios';
+import { showAlert } from './alerts';
+const stripe = Stripe(
+  'pk_test_51IaMGJSJ3QMXvUJtoQQCDvIjSBTWIhcyDkuKqwr8FQnkctVerNedUBRr28aktq6dLQaokHvAIj8RFwQdChTBvrFP00io0L5G8L'
+);
+
+export const bookTour = async tourId => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    //console.log(session);
+
+    // 2) Create checkout form + chanre credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
---
+++ readme.md
@@ -0,0 +1,5 @@
+#Natures Applications
+
+The Natures Application consist of nine tours in different cities where you can book those Tours brefore booking you can also see the Reviews,Ratings,and some Photos of the Tour.
+You can also see the location where your going on day1 to day7 or based on the tour duration.
+there is a map that will locates those location
