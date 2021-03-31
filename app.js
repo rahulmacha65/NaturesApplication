98c42665c5b61015013c915b6603277cba744be1
rahulmacha65 <rahul.macha567@gmail.com>
2021-03-31 23:10:04 +0530

    Implemented cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require("xss-clean");
 const hpp = require("hpp");
 const cookieParser = require("cookie-parser");
 const compression = require("compression");
+const cors = require("cors");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -25,6 +26,20 @@ app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 // 1) GLOBAL MIDDLEWARES
+//implemet cors It allow all Access-control-allow-Origin
+app.use(cors());
+//api.nataures.com, fornt-end natours.com
+// app.use(
+//   cors({
+//     origin: "https://www.natours.com",
+//   })
+// );
+
+app.options("*", cors());
+
+//allowing for only one Router
+//app.options('/api/v1/tours',cors())
+
 // Serving static files
 app.use(express.static(path.join(__dirname, "public")));
 
